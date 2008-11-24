beaabd880b6a3fcc53251a095ce2ce9ceb0bf328
dave <dave@99f6bc7e-7112-0410-9ef5-d8d0ed8bfc8b>
2008-11-24 08:15:56 +0000

    work without any slave_synonym_ref_chars
    git-svn-id: https://pacific.thinktank.co.nz/svn-freelex/trunk@45 99f6bc7e-7112-0410-9ef5-d8d0ed8bfc8b

--- lib/FreelexDB/Headword/Fields/SLAVESYNONYMS.pm
+++ lib/FreelexDB/Headword/Fields/SLAVESYNONYMS.pm
@@ -5,8 +5,13 @@ sub format_SLAVESYNONYMS_plain {
    foreach my $entry ( FreelexDB::Headword->search( mastersynonymheadwordid => $self->headwordid, { order_by => 'collateseq, variantno, majsense, minsense' } ) ) {
       push @synlist, $self->printedreference($entry->headwordid);
    }
+
    if (@synlist) { 
-      return FreelexDB::Globals->slave_synonym_ref_chars->[0] . join(', ', @synlist) . FreelexDB::Globals->slave_synonym_ref_chars->[1];
+      my $left = exists &{"FreelexDB::Globals::slave_synonym_ref_chars"} && 
+                 defined FreelexDB::Globals->slave_synonym_ref_chars->[0] ? FreelexDB::Globals->slave_synonym_ref_chars->[0] : "";
+      my $right = exists &{"FreelexDB::Globals::slave_synonym_ref_chars"} &&
+                  defined FreelexDB::Globals->slave_synonym_ref_chars->[1] ? FreelexDB::Globals->slave_synonym_ref_chars->[1] : "";
+      return $left . join(', ', @synlist) . $right;
    }
    else { return "" }
 }
