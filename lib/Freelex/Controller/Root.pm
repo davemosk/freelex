7fc9123d0e575d513f73b8751ee348339e6a3fb7
Dave Moskovitz <dave@thinktank.co.nz>
2012-03-16 18:00:51 +1300

    add security check against external hosts accessing catalyst server directly

--- lib/Freelex/Controller/Root.pm
+++ lib/Freelex/Controller/Root.pm
@@ -6,11 +6,32 @@ use strict;
     __PACKAGE__->config( namespace => '');
 
 
+sub auto : Private {
+     my ( $self, $c ) = @_;
+     if ($c->request->address =~ /^127\.0\./ || $c->request->headers->{"x-forwarded-server"} =~ /^127\.0\./ ) {
+         return 1;
+     }
+     else {
+         $c->response->status( 404 );
+         $c->stash->{template} = '404.tt';
+         $c->response->headers->header( 'X-Catalyst' => undef );
+         $c->detach('Freelex::View::TT');
+         return 0;
+      }
+}
+
+
 sub default : Private {
     my ( $self, $c ) = @_;
 
 # default method if no path specified
-     $c->forward('/main/freelex');
+
+     if ($c->user) {
+        $c->forward('/main/freelex');
+     }
+     else {
+        $c->forward('/login/login');
+     }
      $c->detach('Freelex::View::TT');
 #    $c->response->body( "Sorry, we can't service your request." );
 #    $c->response->status(404);
